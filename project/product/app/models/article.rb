f608033d72d63b24c7ba0d313e3130939f7e8c43
amangautamberyl <aman.gautam@berylsystems.com>
2016-12-13 18:52:36 +0530

    13-dec-data

---
+++ project/product/app/models/article.rb
@@ -0,0 +1,6 @@
+class Article < ApplicationRecord
+	validates :title, :description, :image_url, presence: true
+	validates :description, uniqueness: true  
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
+	validates :image_url, allow_blank: true, format: {with:%r{\.(gif|jpg|png|jpeg)\Z}i,message: 'must be a URL for GIF, JPG, JPEG or PNG image.'}
+end
